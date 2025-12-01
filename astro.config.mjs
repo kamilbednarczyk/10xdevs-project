58f03d61134023a158a79e12269adc7594f696fc
Kamil <kamil2.bednarczyk@gmail.com>
2025-12-01 12:35:25 +0100

    chore: Add cloudflare related configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 const WATCH_IGNORE = [
   "**/coverage/**",
@@ -27,7 +27,5 @@ export default defineConfig({
       },
     },
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
